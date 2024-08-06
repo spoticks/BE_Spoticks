c1f8bee1d348f6bdedfd117140766506d5ded7fa
Jihyo <jh04187@gmail.com>
2024-08-06 19:39:14 +0900

    :bug: fix: Member id 자동 생성 어노테이션 추가

--- src/main/java/spoticks/ticket_reservation/domain/member/entity/Member.java
+++ src/main/java/spoticks/ticket_reservation/domain/member/entity/Member.java
@@ -13,6 +13,7 @@ public class Member extends BaseTimeEntity {
 
     @Id
     @Column(name = "member_id", updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, updatable = false)
