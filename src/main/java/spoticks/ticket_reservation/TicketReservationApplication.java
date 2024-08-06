4857caf4fd6bff8c2ed84c157c49acc7836fc769
Jihyo <jh04187@gmail.com>
2024-08-06 17:19:02 +0900

    :sparkles: feat: 생성 및 변경 시간 자동 설정 클래스 구현 #1

--- src/main/java/spoticks/ticket_reservation/TicketReservationApplication.java
+++ src/main/java/spoticks/ticket_reservation/TicketReservationApplication.java
@@ -2,7 +2,9 @@ package spoticks.ticket_reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TicketReservationApplication {
 
---
+++ src/main/java/spoticks/ticket_reservation/global/common/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package spoticks.ticket_reservation.global.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.DATE)
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+}
