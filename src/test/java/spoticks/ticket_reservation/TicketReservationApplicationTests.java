69df93c07d3de9df4101d1140089cc87cec53442
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-08-06 16:13:47 +0900

    ✨ feat: 초기설정 완료

---
+++ src/test/java/spoticks/ticket_reservation/TicketReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package spoticks.ticket_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
