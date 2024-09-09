7201ad1df1ba120bfcdf93e3672a367f9328cd32
Jihyo <jh04187@gmail.com>
2024-09-09 17:26:14 +0900

    :sparkles: feat: 에러코드 추가 #6

--- src/main/java/spoticks/ticket_reservation/global/error/ErrorCode.java
+++ src/main/java/spoticks/ticket_reservation/global/error/ErrorCode.java
@@ -6,16 +6,22 @@ import lombok.Getter;
 public enum ErrorCode {
 
     // Common
-    INVALID_INPUT_VALUE(400, " Invalid Input Value"),
-    INVALID_TYPE_VALUE(400, " Invalid Type Value"),
-    ENTITY_NOT_FOUND(400, " Entity Not Found"),
+    INVALID_INPUT_VALUE(400, "Invalid Input Value"),
+    INVALID_TYPE_VALUE(400, "Invalid Type Value"),
+    ENTITY_NOT_FOUND(404, " Entity Not Found"),
     INTERNAL_SERVER_ERROR(500, "Server Error"),
     HANDLE_ACCESS_DENIED(403, "Access is Denied"),
 
-    // User
+    // Member
+    MEMBER_NOT_FOUND(404, "Member Not Found"),
     EMAIL_DUPLICATION(400, "Email is Duplication"),
     PHONE_DUPLICATION(400, "Phone Number is Duplication"),
     USERNAME_DUPLICATION(400, "LoginId is Duplication"),
+
+    // Game
+    GAME_DELETION_FAILED(400, "Game Deletion Failed"),
+    GAME_SAVE_FAILED(400, "Game Save Failed"),
+    GAME_NOT_FOUND(404, "Game Not Found"),
 
     // Reservation
     RESERVATION_EXPIRE(400, "Reservation already expired"),
