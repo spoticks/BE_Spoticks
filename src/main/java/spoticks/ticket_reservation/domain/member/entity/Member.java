2f15451a19eb18d20f4966d22df09394eb9be6c0
Jihyo <jh04187@gmail.com>
2024-10-28 19:35:12 +0900

    :bug: fix: member 생성 시 역할 디폴트 값으로 ROLE_MEMBER 추가

--- src/main/java/spoticks/ticket_reservation/domain/member/entity/Member.java
+++ src/main/java/spoticks/ticket_reservation/domain/member/entity/Member.java
@@ -9,7 +9,6 @@ import spoticks.ticket_reservation.domain.member.dto.MemberDto;
 import spoticks.ticket_reservation.domain.team.entity.Team;
 import spoticks.ticket_reservation.global.common.BaseTimeEntity;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -49,7 +48,7 @@ public class Member extends BaseTimeEntity {
     private Set<Team> teams;
 
     @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "member_id"))
     @Column(name = "role")
     private List<String> roles;
 
@@ -61,7 +60,7 @@ public class Member extends BaseTimeEntity {
         this.phoneNumber = phoneNumber;
         this.status = MemberStatus.ACTIVE;
         this.teams = new HashSet<>();
-        this.roles = new ArrayList<>();
+        this.roles = List.of("ROLE_MEMBER");
     }
 
     public void updateMemberInfo(MemberDto.ModifyPhoneReq dto) {
