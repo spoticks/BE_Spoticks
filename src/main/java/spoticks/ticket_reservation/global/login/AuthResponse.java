bcfbd327bc37f513989ffc4157be132fc2843134
Jihyo <jh04187@gmail.com>
2024-11-14 20:43:50 +0900

    :bug: fix: 토큰 페이로드에 유저 정보를 추가함에 따라 로그인 response 간소화 #10

--- src/main/java/spoticks/ticket_reservation/global/login/AuthResponse.java
+++ src/main/java/spoticks/ticket_reservation/global/login/AuthResponse.java
@@ -1,16 +1,12 @@
 package spoticks.ticket_reservation.global.login;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 @AllArgsConstructor
 public class AuthResponse {
 
-    private long memberId;
-    private String memberName;
     private String token;
 
 }
