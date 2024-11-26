91bbc55bf8d185c95d6c8f270f099df6c8c5714f
Jihyo <jh04187@gmail.com>
2024-11-27 01:55:26 +0900

    :sparkles: feat: 접근 제한을 다루는 Exception 구현 #20

---
+++ src/main/java/spoticks/ticket_reservation/global/error/exception/AccessDeniedException.java
@@ -0,0 +1,11 @@
+package spoticks.ticket_reservation.global.error.exception;
+
+import spoticks.ticket_reservation.global.error.ErrorCode;
+
+public class AccessDeniedException extends BusinessException {
+
+    public AccessDeniedException(String message) {
+        super(message, ErrorCode.HANDLE_ACCESS_DENIED);
+    }
+
+}
