c4c8f0ed42bbd6d77f710a48f229455e194a0434
Jihyo <jh04187@gmail.com>
2024-12-03 23:29:05 +0900

    :sparkles: feat: page not found 에러 코드 추가 #21

--- src/main/java/spoticks/ticket_reservation/global/error/ErrorCode.java
+++ src/main/java/spoticks/ticket_reservation/global/error/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(400, "Invalid Input Value"),
     INVALID_TYPE_VALUE(400, "Invalid Type Value"),
     UNAUTHORIZED(401, "Invalid username or password"),
-    ENTITY_NOT_FOUND(404, " Entity Not Found"),
+    ENTITY_NOT_FOUND(404, "Entity Not Found"),
+    PAGE_NOT_FOUND(404, "Requested endpoint does not exist"),
     INTERNAL_SERVER_ERROR(500, "Server Error"),
     HANDLE_ACCESS_DENIED(403, "Access is Denied"),
 
