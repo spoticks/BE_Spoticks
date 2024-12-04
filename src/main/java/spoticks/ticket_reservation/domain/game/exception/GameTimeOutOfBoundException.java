b9d6e428ed91f9bcc96b266289147d22c2d584b7
Jihyo <jh04187@gmail.com>
2024-12-04 21:21:18 +0900

    :bug: fix: Exception 상황에 맞는 커스텀 에러코드 적용  #23

--- src/main/java/spoticks/ticket_reservation/domain/game/exception/GameTimeOutOfBoundException.java
+++ src/main/java/spoticks/ticket_reservation/domain/game/exception/GameTimeOutOfBoundException.java
@@ -1,9 +1,10 @@
 package spoticks.ticket_reservation.domain.game.exception;
 
-import spoticks.ticket_reservation.global.error.exception.InvalidValueException;
+import spoticks.ticket_reservation.global.error.ErrorCode;
+import spoticks.ticket_reservation.global.error.exception.BusinessException;
 
-public class GameTimeOutOfBoundException extends InvalidValueException {
+public class GameTimeOutOfBoundException extends BusinessException {
     public GameTimeOutOfBoundException() {
-        super("Time is out of bound");
+        super("Time to access this game has ended", ErrorCode.RESERVATION_NOT_AVAILABLE);
     }
 }
