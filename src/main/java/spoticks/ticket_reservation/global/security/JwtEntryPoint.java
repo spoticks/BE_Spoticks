16d66f71269945554490a763eb283b2ccb622d5c
Jihyo <jh04187@gmail.com>
2024-12-12 20:26:39 +0900

    :truck: rename: 불필요한 import 항목 삭제

--- src/main/java/spoticks/ticket_reservation/global/security/JwtEntryPoint.java
+++ src/main/java/spoticks/ticket_reservation/global/security/JwtEntryPoint.java
@@ -1,7 +1,6 @@
 package spoticks.ticket_reservation.global.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
