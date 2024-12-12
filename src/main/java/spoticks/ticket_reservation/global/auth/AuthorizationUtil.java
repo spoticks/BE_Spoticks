3c13f875c002090e22243389e4e068e590f1e96a
Jihyo <jh04187@gmail.com>
2024-12-12 20:20:33 +0900

    :heavy_plus_sign: add: util 클래스에 component 애너테이션 추가

--- src/main/java/spoticks/ticket_reservation/global/auth/AuthorizationUtil.java
+++ src/main/java/spoticks/ticket_reservation/global/auth/AuthorizationUtil.java
@@ -2,12 +2,10 @@ package spoticks.ticket_reservation.global.auth;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthorizationUtil {
-
-    private AuthorizationUtil() {
-        throw new AssertionError();
-    }
 
     public static Long getMemberId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
