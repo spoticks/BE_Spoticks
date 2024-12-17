ca564545c2b20a5b3d1cbd5976b621ee4a5a9be7
Jihyo <jh04187@gmail.com>
2024-12-18 00:12:28 +0900

    :bug: fix: 불필요한 생성자 삭제 및 신규 생성자 추가

--- src/main/java/spoticks/ticket_reservation/global/error/exception/InvalidValueException.java
+++ src/main/java/spoticks/ticket_reservation/global/error/exception/InvalidValueException.java
@@ -4,8 +4,8 @@ import spoticks.ticket_reservation.global.error.ErrorCode;
 
 public class InvalidValueException extends BusinessException {
 
-    public InvalidValueException(String message) {
-        super(message, ErrorCode.INVALID_INPUT_VALUE);
+    public InvalidValueException(ErrorCode errorCode) {
+        super(errorCode);
     }
 
     public InvalidValueException(String message, ErrorCode errorCode) {
