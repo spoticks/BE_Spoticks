2091d33ffddc763ace26d1587b289fbb920e3b2e
Jihyo <jh04187@gmail.com>
2025-01-10 00:22:41 +0900

    :bug: fix: requestMatchers 를 ant 패턴형식에 맞게 수정 #33

--- src/main/java/spoticks/ticket_reservation/global/config/SecurityConfig.java
+++ src/main/java/spoticks/ticket_reservation/global/config/SecurityConfig.java
@@ -46,12 +46,12 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/reissue/{username}", "/join", "/join/**",
-                                "/teams/{teamId}/games", "teams/{teamId}/stadium",  "/games/mostPopular", "/games/weekly", "/games/sports").permitAll()
-                        .requestMatchers("/members", "/members/me", "/members/password", "/myteam", "/myteam/{teamId}",
-                                "/reservation", "/reservation/{reservationId}",
-                                "/games/{gameId}/preempt", "/games/{gameId}/reserve", "/games/{gameId}",
-                                "/auth/logout", "/admin/games/**").authenticated()
+                        .requestMatchers("/auth/login", "/auth/reissue/*", "/join", "/join/phone-number", "/join/user-name",
+                                "/teams/*/games", "/teams/*/stadium",  "/games/mostPopular", "/games/weekly", "/games/sports").permitAll()
+                        .requestMatchers("/members", "/members/me", "/members/password", "/my-team", "/my-team/*",
+                                "/reservation", "/reservation/*",
+                                "/games/*/preempt", "/games/*/reserve", "/games/*",
+                                "/auth/logout", "/admin/games", "/admin/games/*").authenticated()
                         .anyRequest().denyAll())
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtEntryPoint))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
